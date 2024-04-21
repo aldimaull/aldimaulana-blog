916858d0574fced6b9df92ebe31ad8a4e4d6ff45
Aldi Maulana <aldimaul@upi.edu>
2024-04-21 18:28:38 +0700

    vercel fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://aldimaulana.vercel.app/",
-  integrations: [react(), tailwind(), sitemap(), mdx()]
+  integrations: [react(), tailwind(), sitemap(), mdx()],
 });
